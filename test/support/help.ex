aa94510da12c03469830e4d5df657f33c3f7c277
ruslandoga <ruslandoga+gh@icloud.com>
2026-07-21 15:09:47 +0300

    add req

---
+++ test/support/help.ex
@@ -0,0 +1,25 @@
+defmodule Help do
+  def s3_credentials(:minio) do
+    %{
+      access_key_id: System.get_env("AWS_ACCESS_KEY_ID", "minioadmin"),
+      secret_access_key: System.get_env("AWS_SECRET_ACCESS_KEY", "minioadmin"),
+      region: System.get_env("AWS_REGION", "us-east-1"),
+      endpoint_url: System.get_env("AWS_ENDPOINT_URL_S3", "http://localhost:9000")
+    }
+  end
+
+  def s3_req(credentials) do
+    req =
+      Req.new(
+        aws_sigv4: [
+          service: :s3,
+          access_key_id: credentials[:access_key_id],
+          secret_access_key: credentials[:secret_access_key],
+          region: credentials[:region]
+        ],
+        retry: :transient
+      )
+
+    ReqS3.attach(req, aws_endpoint_url_s3: credentials[:endpoint_url])
+  end
+end
--- test/w3_test.exs
+++ test/w3_test.exs
@@ -1,8 +1,21 @@
 defmodule W3Test do
-  use ExUnit.Case
-  doctest W3
+  use ExUnit.Case, async: true
 
-  test "greets the world" do
-    assert W3.hello() == :world
+  setup_all do
+    s3_credentials = Help.s3_credentials(:minio)
+    s3_req = Help.s3_req(s3_credentials)
+    {:ok, s3_req: s3_req}
+  end
+
+  test "s3 create bucket, put and get object, delete bucket", %{s3_req: s3_req} do
+    # Create bucket and delete on exit
+    on_exit(fn -> Req.delete!(s3_req, url: "s3://w3-test-bucket") end)
+    assert Req.put!(s3_req, url: "s3://w3-test-bucket")
+
+    # Put object
+    assert Req.put!(s3_req, url: "s3://w3-test-bucket/test-key", body: "Hello, S3!")
+
+    # Get object
+    assert Req.get!(s3_req, url: "s3://w3-test-bucket/test-key").body == "Hello, S3!"
   end
 end
