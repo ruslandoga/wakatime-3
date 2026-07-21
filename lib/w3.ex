ca2905280ff1e2062a33fcb2d48f045f24efe100
ruslandoga <ruslandoga+gh@icloud.com>
2026-07-21 15:59:44 +0300

    cleanup

--- lib/w3.ex
+++ lib/w3.ex
@@ -2,17 +2,4 @@ defmodule W3 do
   @moduledoc """
   Documentation for `W3`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> W3.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
